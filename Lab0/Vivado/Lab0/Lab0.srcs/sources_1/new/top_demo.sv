6849867fb1eaafcf33b7f24f753075144bb29ade
Varney <wvarney@okstate.edu>
2023-02-08 16:42:14 -0600

    finish lab

--- Lab0/Vivado/Lab0/Lab0.srcs/sources_1/new/top_demo.sv
+++ Lab0/Vivado/Lab0/Lab0.srcs/sources_1/new/top_demo.sv
@@ -26,8 +26,16 @@ module top_demo
   input  logic [3:0] btn,
   output logic [7:0] led
 );
+ lab0 dut (.a(sw[0]), .b(sw[1]), .cin(sw[2]), .s(led[0]),.cout(led[1]));
 
-  assign led[3:0] = sw;
-  assign led[7:4] = btn;
+endmodule
+module lab0 (input  logic a, b, cin, 
+output logic s, cout);
+logic p, g;   
+assign p = a ^ b;
+assign g = a & b;
+  
+assign s = p ^ cin;
+assign cout = g | (p & cin);
 
 endmodule
